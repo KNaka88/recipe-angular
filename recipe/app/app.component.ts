372f59850b177aa4f0e012554c4e9d4781943c30
Koji Nakagawa <lightupthesky0627@gmail.com>
2017-03-28 23:16:29 -0700

    add partials folder and create templateUrl

--- recipe/app/app.component.ts
+++ recipe/app/app.component.ts
@@ -2,39 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-  <h1>Recipe Website</h1>
-    <h3>Recipe</h3>
-    <ul>
-      <li (click)="showDetail()" (click)="selectRecipe(currentRecipe)" *ngFor="let currentRecipe of recipes">
-        {{currentRecipe.title}}
-      </li>
-    </ul>
-
-      <div *ngIf="showHide">
-        <h3>How to Cook {{selectedRecipe.title}}</h3><br>
-        <p>Ingredients: {{selectedRecipe.ingredients}}</p><br>
-        <p>Directions: {{selectedRecipe.directions}}</p><br>
-        <form>
-          <div class="form-group">
-            <label for="title">Edit Recipe Name</label>
-            <input name="editTitle" [(ngModel)]="editTitle" type="text" id="title">
-          </div>
-          <div class="form-group">
-            <label for="ingredients">Edit Recipe ingredients</label>
-            <input name="editIngredients" [(ngModel)]="editIngredients" type="text" id="ingredients">
-          </div>
-          <div class="form-group">
-            <label for="directions">Edit Recipe Directions</label>
-            <input name="editDirections" [(ngModel)]="editDirections" type="text" id="directions">
-          </div>
-        </form>
-        <p>{{editTitle}}</p>
-        <button (click)="saveRecipe(editTitle, editIngredients, editDirections)">Save</button>
-      </div>
-
-
-  `
+  templateUrl: 'partials/app.html'
 })
 
 export class AppComponent {
